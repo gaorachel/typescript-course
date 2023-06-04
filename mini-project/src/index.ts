41b52a5e690ebc1bf925c01021ee20d60c61767b
Rachel <gggggrea@gmail.com>
2023-06-04 23:15:32 +0100

    Create a mini project

---
+++ mini-project/src/index.ts
@@ -0,0 +1,66 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const btn = document.getElementById("btn") as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("#todoform")!;
+const list = document.getElementById("todolist")!;
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
+  saveTodos();
+
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", () => {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+  newLI.append(todo.text);
+  newLI.append(checkbox);
+  list.append(newLI);
+}
+
+form.addEventListener(
+  "submit",
+  handleSubmit
+  // (e) => {
+  //   e.preventDefault();
+  //   console.log("submitted!");
+  // }
+);
+
+// btn?.addEventListener("click", () => {
+//   //   alert("clicked!");
+//   alert(input.value);
+//   input.value = ""; // reset the input place
+// });
