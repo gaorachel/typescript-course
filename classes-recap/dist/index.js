933f0ccbe5a96ffc23734e1113881edf1d0f6884
Rachel <gggggrea@gmail.com>
2023-06-05 21:46:16 +0100

    Learn some classes in ts

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("xxx");
+// how to do it in traditional js:
+// class Player {
+//   constructor(first, last) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   get fullName() {
+//     return `${this.first} ${this.last}`;
+//   }
+//   taunt() {
+//     console.log("booyah!");
+//   }
+// }
+// const player1 = new Player("rachel", "gao");
+// console.log(player1.fullName);
+// console.log(player1.first);
+// one way of ts
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score: number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+// }
+//another way
+class Player {
+    constructor(first, last, score = 0) {
+        this.first = first;
+        this.last = last;
+        this.score = score;
+    }
+}
+const player1 = new Player("Rachel", "Gao");
+console.log(player1);
--- classes-recap/src/index.ts
+++ classes-recap/src/index.ts
@@ -1,1 +1,38 @@
-console.log("xxx");
+// how to do it in traditional js:
+// class Player {
+//   constructor(first, last) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   get fullName() {
+//     return `${this.first} ${this.last}`;
+//   }
+//   taunt() {
+//     console.log("booyah!");
+//   }
+// }
+
+// const player1 = new Player("rachel", "gao");
+
+// console.log(player1.fullName);
+// console.log(player1.first);
+
+// one way of ts
+// class Player {
+//   public readonly first: string;
+//   public readonly last: string;
+//   private score: number = 0;
+
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+// }
+
+//another way
+class Player {
+  constructor(public readonly first: string, public readonly last: string, private score: number = 0) {}
+}
+
+const player1 = new Player("Rachel", "Gao");
+console.log(player1);
