a2f343b04f632aafb1f0101d160d1a3b9e4e55bf
Rachel <gggggrea@gmail.com>
2023-06-08 21:04:57 +0100

    Set up a react app with ts

---
+++ shopping-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Greeter } from "./components/Greeter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
